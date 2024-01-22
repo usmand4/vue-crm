eb26e69d4d3ff283b45ab07591224eada2887517
Muhammad Usman Danish <usmanalid4@gmail.com>
2024-01-22 12:17:37 +0500

    Fix line endings

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-app>
+    <v-main>
+      <LoginPage/>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import LoginPage from './components/pages/Login.vue'
+
+export default {
+  name: 'App',
+
+  components: {
+    LoginPage,
+  },
+}
+</script>
